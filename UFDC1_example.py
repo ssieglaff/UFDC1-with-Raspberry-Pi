8b9b380c1e6c5c150a72a5f82c5a57072e54c105
Sander Sieglaff <49599108+ssieglaff@users.noreply.github.com>
2021-12-18 15:16:20 -0600

    Update UFDC1_example.py
    Corrected the logic to wait until the measurement is completed before asking for the result.

--- UFDC1_example.py
+++ UFDC1_example.py
@@ -9,7 +9,7 @@ print("Measuring mode set to " + str(ufdc.get_measuring_mode()))
 
 ufdc.start_measurement()
 
-while(ufdc.is_measurement_done()):
+while(not ufdc.is_measurement_done()):
 	pass
 
 print("Result of measurement: " + str(ufdc.get_result_decimal()) + " Hz")
